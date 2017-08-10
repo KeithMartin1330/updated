7c76de61ac040212cd7de28e1e0f5cba3c982e08
keithmartin1330 <keithsdevelopment>
2017-08-10 11:57:08 +0100

    Updated git

---
+++ src/main/java/com/fenergo/pull_from_repository_deploy_to_server/App.java
@@ -0,0 +1,13 @@
+package com.fenergo.pull_from_repository_deploy_to_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!dfgdfg" );
+    }
+}
